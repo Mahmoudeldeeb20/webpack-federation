27c324c891b0d3d1d6212ddf8d2aebc75c1fcbd9
Mahmoud Hamdy <Mahmoud-El-Deeb@hotmail.com>
2023-07-11 05:58:24 +0300

    try and run webpack federation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Suspense } from "react";
+const Todo = React.lazy(() => import("todo/App"));
+const Signup = React.lazy(() => import("signup/App"));
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<nav className="navbar navbar-expand-lg bg-body-tertiary">
+				<div className="container">
+					<a className="navbar-brand" href="/">
+						Webpack Federation
+					</a>
+					<div className="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+							<li className="nav-item">
+								<a className="nav-link active" href="/todo">
+									To Do
+								</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="/signup">
+									Sign-Up
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+			<div>
+				<Suspense fallback={<div>Loading . . .</div>}>
+					<Todo />
+					<Signup />
+				</Suspense>
+			</div>
+		</div>
+	);
 }
 
 export default App;
+
